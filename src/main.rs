c06bac8af7b380ad3a4562b11572e3d68116a54c
Jamie Hardt <jamiehardt@me.com>
2021-11-26 17:27:17 -0800

    Update main.rs
    Style cleanup and changed interface

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-extern crate cfb;
-extern crate encoding;
 
 use encoding::all::UTF_16LE;
 use encoding::{Encoding, DecoderTrap};
@@ -26,56 +24,56 @@ fn print_object<T>(file : &mut AAFFile<T>, obj: &InterchangeObjectDescriptor)
             
         if descend {
             println!("{}Object({:?}) {{",indent_str, obj.path);
-            for prop in file.raw_properties(obj) {
-                let val = file.resolve_property_value(obj, &prop);
+            for pid in file.all_property_ids(obj) {
+                let val = file.get_value(obj, pid);
 
                 match val {
                     PropertyValue::Data(v) => {
-                        match prop.pid {
+                        match pid {
                             0x06 | 0x1b02 => {
                                 let sval = UTF_16LE.decode(&*v, DecoderTrap::Strict)
                                     .unwrap_or(String::from("(Undecodable)"));
-                                println!("  {}(pid {:#04x}) = string({})", indent_str, prop.pid, sval); 
+                                println!("  {}(pid {:#04x}) = string({})", indent_str, pid, sval); 
                             },
                             _ => { 
-                                println!("  {}(pid {:#04x}) = len({:?})", indent_str, prop.pid, v.len()); 
+                                println!("  {}(pid {:#04x}) = len({:?})", indent_str, pid, v.len()); 
                             }
                         }
                     }
                     PropertyValue::Stream(p) => {
-                        println!("  {}(pid {:#04x}) = stream({:?})", indent_str, prop.pid, p);
+                        println!("  {}(pid {:#04x}) = stream({:?})", indent_str, pid, p);
                     },
                     PropertyValue::Single(o) => {
-                        println!("  {}(pid {:#04x}) => ", indent_str, prop.pid);
+                        println!("  {}(pid {:#04x}) => ", indent_str, pid);
                         print_obj_impl(file, &o, indent + 4, true);
                         println!("  {}", indent_str);
                     },
                     PropertyValue::Vector(o) => {
-                        println!("  {}(pid {:#04x}) = [", indent_str, prop.pid);
+                        println!("  {}(pid {:#04x}) = [", indent_str, pid);
                         for child in o {
                             print_obj_impl(file, &child, indent + 4, true);
                         }
                         println!("  {}]", indent_str);
                     },
                     PropertyValue::Set(o) => {
-                        println!("  {}(pid {:#04x}) = (", indent_str, prop.pid);
+                        println!("  {}(pid {:#04x}) = (", indent_str, pid);
                         for child in o {
                             print_obj_impl(file, &child, indent + 4, true);
                         }
                         println!("  {})", indent_str);
                     },
                     PropertyValue::Reference(o) => {
-                        println!("  {}(pid {:#04x}) ~> Ref({:?})", indent_str, prop.pid, o.path);
+                        println!("  {}(pid {:#04x}) ~> Ref({:?})", indent_str, pid, o.path);
                     },
                     PropertyValue::ReferenceVector(o) => {
-                        println!("  {}(pid {:#04x}) ~> [", indent_str, prop.pid);
+                        println!("  {}(pid {:#04x}) ~> [", indent_str, pid);
                         for child in o {
                             println!("    {}Ref({:?})", indent_str, child.path);
                         }
                         println!("  {}]", indent_str);
                     },
                     PropertyValue::ReferenceSet(o) => {
-                        println!("  {}(pid {:#04x}) ~> [", indent_str, prop.pid);
+                        println!("  {}(pid {:#04x}) ~> [", indent_str, pid);
                         for child in o {
                             println!("    {}Ref({:?})", indent_str, child.path);
                         }
