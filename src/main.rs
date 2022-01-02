5fe6469836c149685270579d2c67df50dbb9f8a1
Jamie Hardt <jamiehardt@me.com>
2022-01-02 13:00:36 -0800

    RustFmt

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,36 @@
-
 use encoding::all::UTF_16LE;
-use encoding::{Encoding, DecoderTrap};
+use encoding::{DecoderTrap, Encoding};
 
+mod aaf;
+mod file;
 mod interchange_object;
 mod properties;
-mod types;
-mod file;
 mod session;
-mod aaf;
+mod types;
 
-use crate::interchange_object::InterchangeObjectDescriptor;
 use crate::file::AAFFile;
+use crate::interchange_object::InterchangeObjectDescriptor;
 use std::io::{Read, Seek};
 
+fn print_object<T>(file: &mut AAFFile<T>, obj: &InterchangeObjectDescriptor)
+where
+    T: Read + Seek,
+{
+    let i = file.walk_properties();
 
-fn print_object<T>(file : &mut AAFFile<T>, obj: &InterchangeObjectDescriptor)
-    where T: Read + Seek {
-    
-        let i = file.walk_properties();
+    for entry in i {
+        let indent = std::iter::repeat("  ")
+            .take(entry.depth())
+            .collect::<String>();
 
-        for entry in i {
-            let indent = std::iter::repeat("  ")
-                .take(entry.depth())
-                .collect::<String>();
-
-            println!("{}Parent: {:?}", indent, entry.parent().path);
-            println!("{}Prop: {}", indent, entry.property_id());
-        }
+        println!("{}Parent: {:?}", indent, entry.parent().path);
+        println!("{}Prop: {}", indent, entry.property_id());
+    }
 }
 
 fn main() {
-    let test_path =  "testmedia/AAF_Test_1/AAF_Test_1.aaf";
-    let mut f = AAFFile::open(test_path)
-        .expect("error opening file"); 
+    let test_path = "testmedia/AAF_Test_1/AAF_Test_1.aaf";
+    let mut f = AAFFile::open(test_path).expect("error opening file");
 
     let root = f.root_object();
     print_object(&mut f, &root);
