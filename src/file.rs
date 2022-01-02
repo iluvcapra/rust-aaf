bed9ac422081ec495a4f8e1571f1ad442c244352
Jamie Hardt <jamiehardt@me.com>
2022-01-02 15:56:30 -0800

    Refactoring

--- src/file.rs
+++ src/file.rs
@@ -12,96 +12,11 @@ use crate::aaf::classes::{AAFObject, Header};
 use crate::interchange_object::InterchangeObjectDescriptor;
 use crate::properties::*;
 use crate::types::*;
+use crate::property_iterator::AAFPropertyIterator;
 
 const AAF_FILE_HEADER_PID: OMPropertyId = 0x0002;
 const AAF_FILE_METADICTIONARY_PID: OMPropertyId = 0x0001;
 // AAF File uuid b3b398a5-1c90-11d4-8053-080036210804
-
-pub struct AAFEntry {
-    parent: InterchangeObjectDescriptor,
-    property: OMPropertyId,
-    value: Option<PropertyValue>,
-    depth: usize,
-}
-
-impl AAFEntry {
-    pub fn parent(&self) -> &InterchangeObjectDescriptor {
-        &self.parent
-    }
-
-    pub fn property_id(&self) -> OMPropertyId {
-        self.property.clone()
-    }
-
-    pub fn value(&self) -> &Option<PropertyValue> {
-        &self.value
-    }
-
-    pub fn depth(&self) -> usize {
-        self.depth.clone()
-    }
-}
-
-pub struct AAFPropertyIterator<'a, F> {
-    file: &'a mut AAFFile<F>,
-    stack: Vec<AAFEntry>,
-}
-
-impl<'a, F> AAFPropertyIterator<'a, F>
-where
-    F: Read + Seek,
-{
-    pub(crate) fn new(file: &'a mut AAFFile<F>, root_object: InterchangeObjectDescriptor) -> Self {
-        let mut retval = AAFPropertyIterator {
-            file,
-            stack: vec![],
-        };
-
-        retval.fill_stack(&root_object, 0);
-        retval
-    }
-
-    fn fill_stack(&mut self, parent: &InterchangeObjectDescriptor, depth: usize) {
-        for pid in self.file.all_property_ids(parent) {
-            let pv = self.file.get_value(&parent, pid);
-            self.stack.push(AAFEntry {
-                parent: parent.clone(),
-                property: pid,
-                value: pv,
-                depth: depth + 1,
-            })
-        }
-    }
-}
-
-impl<'a, F> Iterator for AAFPropertyIterator<'_, F>
-where
-    F: Read + Seek,
-{
-    type Item = AAFEntry;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        self.stack.pop().map(move |e| {
-            match &e.value {
-                Some(PropertyValue::Single(obj)) => {
-                    self.fill_stack(&obj, e.depth());
-                }
-                Some(PropertyValue::Vector(list)) => {
-                    for obj in list.into_iter().rev() {
-                        self.fill_stack(&obj, e.depth());
-                    }
-                }
-                Some(PropertyValue::Set(list)) => {
-                    for obj in list.into_iter().rev() {
-                        self.fill_stack(&obj, e.depth());
-                    }
-                }
-                _ => {}
-            }
-            e
-        })
-    }
-}
 
 /// An AAF file.
 pub struct AAFFile<F> {
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,15 @@
-// use encoding::all::UTF_16LE;
-// use encoding::{DecoderTrap, Encoding};
-
 mod aaf;
 mod file;
 mod interchange_object;
 mod properties;
+mod property_iterator;
+mod object_iterator;
 mod session;
 mod types;
 
 use crate::file::AAFFile;
-// use crate::interchange_object::InterchangeObjectDescriptor;
 use std::io::{Read, Seek};
+use crate::object_iterator::InterchangeObjects;
 
 fn print_object<T>(file: &mut AAFFile<T>)
 where
@@ -31,7 +30,7 @@ where
 fn main() {
     let test_path = "testmedia/AAF_Test_1/AAF_Test_1.aaf";
     let mut f = AAFFile::open(test_path).expect("error opening file");
-
+    
     let root = f.root_object();
     print_object(&mut f);
 }
